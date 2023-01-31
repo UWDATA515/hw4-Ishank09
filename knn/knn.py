a021641dd84560457a0debe22d080501fd4f4b63
ishank09 <ishankvasania09@gmail.com>
2023-01-31 12:28:04 -0800

    FINAL Step 1,2. removing main function)

--- knn/knn.py
+++ knn/knn.py
@@ -11,35 +11,38 @@ class KnnRegressionInterface:
 # Implementation        
 class KnnRegression(KnnRegressionInterface):
     def knn_regression(n_neighbors, data, query):
-        print(type(n_neighbors),' ',type(data),' ',type(query))
         try:
             # n_neighbors check
-            if type(n_neighbors) != int:
-                raise ValueError("n_neighbors is not an integer.")
+            if not isinstance(n_neighbors, int):
+                raise TypeError("n_neighbors is not an integer.")
             if n_neighbors <= 0:
                 raise ValueError("n_neighbors should be greater than 0.")
-
+                
+            
             #data check
             if type(data) != np.ndarray:
-                raise ValueError("data should be a numpy array.")
+                raise TypeError("data should be a numpy array.")
             if data.ndim != 2:
                 raise ValueError("data should be a 2-dimensional numpy array.")
-            if data.shape[0] < n_neighbors:
+            m, n = data.shape
+            if m < n_neighbors:
                 raise ValueError("number of samples in data should be at least as large as n_neighbors.")
-            if data.shape[1] < 2:
+            if n < 2:
                 raise ValueError("n+1 should be at least 2.(n should be at least 1)")
-            if not all(data.shape[1] == len(row) for row in data):
+            if not all(n == len(row) for row in data):
                 raise ValueError("all samples in data should have the same number of variables")
+            if not all([x.shape[0] == n - 1 for x in data[:, :-1]]):
+                raise ValueError("all samples must have the same value of n")
             #query check
             if type(query) != np.ndarray:
-                raise ValueError("query should be a numpy array.")
+                raise TypeError("query should be a numpy array.")
             if not np.issubdtype(data.dtype, np.number):
                 raise ValueError("all samples and labels should be numeric.")
             if not np.issubdtype(query.dtype, np.number):
                 raise ValueError("query should be numeric.")
             if query.ndim != 1:
                 raise ValueError("query should be a 1-dimensional numpy array.")
-            if query.shape[0] != data.shape[1] - 1:
+            if query.shape[0] != n - 1:
                 raise ValueError("number of variables in query should be the same as in data.")
 
                 
@@ -58,8 +61,9 @@ class KnnRegression(KnnRegressionInterface):
             
             
             
-            return -1
+            return -1.0
         except ValueError as e:
-            print("Error:", e)
-            return -1
+            raise ValueError("Error:", e)
+            return -1.0
    
+  
